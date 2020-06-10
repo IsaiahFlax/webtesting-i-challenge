5e447947aaf73a072edfd672a93664c3a0f1ad92
IsaiahFlax <flax.isaiah@gmail.com>
2020-06-10 15:06:31 -0600

    repair test works

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,17 @@
 const enhancer = require('./enhancer.js');
 // test away!
+describe('item functions', () => {
+    describe('repair()', () => {
+        it('should return a new item with durability 100', () => {
+            const item = {
+                name: 'some name',
+                enhancement: 20,
+                durability: 99
+            }
+           // const repairedItem = item.durability = 100
+
+            expect(enhancer.repair(item)).toBe(100)
+            console.log('durability:', item.durability)
+        })
+    })
+})
